f1b2c108136d9c87733168432188e7606c181425
karlos <karlos g libral>
2020-03-07 23:07:04 +0100

    Colores con el paso, pero esta para arreglar

--- mySketch.js
+++ mySketch.js
@@ -38,6 +38,10 @@ function setup() {
   colors[1] = color(65, 234, 212, 220);
   colors[2] = color(255, 159, 28, 220);
 
+  colors[3] = color(247, 23, 53, 220);
+  colors[4] = color(255, 255, 255, 220);
+  colors[5] = color(1, 22, 39, 220);
+
   strokeWeight(2);
   angleMode(DEGREES);
   strokeJoin(ROUND);
@@ -72,7 +76,11 @@ function draw() {
   }
 
   for (let j = 0; j < particles.length; j++) {
-    if (particles[j].isFinished && frameCount % 50 == 0) {
+    if (particles[j].isFinished && paso == "default") {
+      if (frameCount % 50 == 0) {
+        particles.splice(j, 1);
+      }
+    } else if (particles[j].isFinished && paso == "uno") {
       particles.splice(j, 1);
     }
   }
@@ -80,7 +88,7 @@ function draw() {
 
 function mousePressed() {
   if (!clicked) {
-    //video.volume(0);
+    video.volume(0);
     video.loop();
     clicked = true;
   }
@@ -126,8 +134,13 @@ function createParticle(keypointx, keypointy, paso) {
   let Y = targetY + randomR * sin(randomTheta);
 
   let dis = dist(targetX, targetY, X, Y);
-  let C = floor(random(3));
-  let Rmax = dis > 55 ? 65 - dis : random(2, 8);
+  let C;
+  if (paso == "default") {
+    C = floor(random(3));
+  } else {
+    C = floor(random(3, 6));
+  }
+  let Rmax = dis > 20 ? 35 - dis : random(2, 8);
 
   particles.push(new Particle(X, Y, C, Rmax, paso));
   particles.push(new Particle(X, Y, C, Rmax, paso));
@@ -172,7 +185,7 @@ function Particle(tmpX, tmpY, tmpC, tmpRmax, paso) {
     fill(colors[this.colorIndex]);
     noStroke();
 
-    this.pos.x = tmpX + this.xNoise; //+ map(noise(this.xNoise), 0, 1, -10, 50);
+    this.pos.x = tmpX + this.xNoise + map(noise(this.xNoise), 0, 1, -10, 50);
 
     if (this.isFill) {
       fill(colors[this.colorIndex]);
@@ -186,16 +199,19 @@ function Particle(tmpX, tmpY, tmpC, tmpRmax, paso) {
     translate(this.pos.x, this.pos.y);
     rotate(this.theta);
 
-    rect(this.r * 2, this.r, this.r, this.r);
+    rect(this.r, this.r, this.r, this.r);
     strokeWeight(3);
-    point(10, 10);
+
+    if (paso == "default") {
+      point(10, 10);
+    }
     pop();
 
     this.theta += this.thetaSpeed;
     if (paso == "default") {
-      this.pos.y -= this.ySpeed;
-    } else {
       this.pos.y += this.ySpeed;
+    } else {
+      this.pos.y -= this.ySpeed;
     }
     //this.pos.x -= this.ySpeed;
     this.xNoise += 0.01;
